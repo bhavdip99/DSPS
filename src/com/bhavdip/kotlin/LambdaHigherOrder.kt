b9548af73411a4e56caa409717dce1955f608c22
bhavd <bhalodia.bhavdip@gmail.com>
2023-02-22 18:00:13 +0530

    Update Lambda expression and high level functions

--- src/com/bhavdip/kotlin/LambdaHigherOrder.kt
+++ src/com/bhavdip/kotlin/LambdaHigherOrder.kt
@@ -10,15 +10,24 @@ fun main() {
         }
     })
 
-    val myLambda = { s: Int -> println("Sum: $s") } // Lambada Expression [Function]
+    val myLambda: (Int) -> Unit = { s: Int -> println("Sum Lambda 1: $s") } // Lambada Expression [Function]
     program.addTwoNumbers(2, 7, myLambda)
+
+//    val myLambda2: (Int, Int) -> Int = { x: Int, y: Int -> x + y } // Lambada Expression [Function]
+//    program.addTwoNum(2, 7, myLambda2) // Type 1
+//    program.addTwoNum(2, 7, { x, y -> x + y }) // Type 2
+    program.addTwoNum(2, 7) { x, y -> x + y } // Type 3
 }
 
 class LambdaHigherOrder {
+    fun addTwoNum(a: Int, b: Int, action: (Int, Int) -> Int) { // High level function with lambda
+        val result = action(a, b) // x + y = a + b = 2 + 7 = 9
+        println("Sum Lambda 2:= $result")
+    }
+
     fun addTwoNumbers(a: Int, b: Int, action: (Int) -> Unit) { // High level function with lambda
         val sum = a + b
-        action(sum) //println("Sum: $s")
-//        println("Sum:= $sum")
+        action(sum) //println("Sum Lambda 1: $s")
     }
 
     fun addTwoNumbers(a: Int, b: Int) { // Using interface/callback Object oriented way
@@ -31,8 +40,6 @@ class LambdaHigherOrder {
         action.add(sum)
         println("Sum:= $sum")
     }
-
-
 }
 
 interface MyAddInterface {
