c082fc86af55c87c16f37b932bd4286ab1ad870c
bhavd <bhalodia.bhavdip@gmail.com>
2023-02-22 18:06:05 +0530

    Update Lambda with closure; return outside variable

--- src/com/bhavdip/kotlin/LambdaHigherOrder.kt
+++ src/com/bhavdip/kotlin/LambdaHigherOrder.kt
@@ -17,18 +17,13 @@ fun main() {
 //    program.addTwoNum(2, 7, myLambda2) // Type 1
 //    program.addTwoNum(2, 7, { x, y -> x + y }) // Type 2
     program.addTwoNum(2, 7) { x, y -> x + y } // Type 3
+
+    var result = 0
+    program.addTwoNum2(2, 7) { x, y -> result = x + y } // Type 3
+    println("Result sum: $result")
 }
 
 class LambdaHigherOrder {
-    fun addTwoNum(a: Int, b: Int, action: (Int, Int) -> Int) { // High level function with lambda
-        val result = action(a, b) // x + y = a + b = 2 + 7 = 9
-        println("Sum Lambda 2:= $result")
-    }
-
-    fun addTwoNumbers(a: Int, b: Int, action: (Int) -> Unit) { // High level function with lambda
-        val sum = a + b
-        action(sum) //println("Sum Lambda 1: $s")
-    }
 
     fun addTwoNumbers(a: Int, b: Int) { // Using interface/callback Object oriented way
         val sum = a + b
@@ -39,6 +34,20 @@ class LambdaHigherOrder {
         val sum = a + b
         action.add(sum)
         println("Sum:= $sum")
+    }
+
+    fun addTwoNumbers(a: Int, b: Int, action: (Int) -> Unit) { // High level function with lambda
+        val sum = a + b
+        action(sum) //println("Sum Lambda 1: $s")
+    }
+
+    fun addTwoNum(a: Int, b: Int, action: (Int, Int) -> Int) { // High level function with lambda
+        val result = action(a, b) // x + y = a + b = 2 + 7 = 9
+        println("Sum Lambda 2:= $result")
+    }
+
+    fun addTwoNum2(a: Int, b: Int, action: (Int, Int) -> Unit) {
+        action(a, b) // result = x + y = a + b = 2 + 7 = 9
     }
 }
 
